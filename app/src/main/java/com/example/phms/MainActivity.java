5854299713a2a8d61fe577ff8aef5eac3236a8b4
giridipika <46011548+giridipika@users.noreply.github.com>
2021-07-20 16:40:38 -0500

    DB connected

--- app/src/main/java/com/example/phms/MainActivity.java
+++ app/src/main/java/com/example/phms/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.phms;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+    // Declare all variable here
+    FirebaseAuth mFirebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
     }
+
 }
