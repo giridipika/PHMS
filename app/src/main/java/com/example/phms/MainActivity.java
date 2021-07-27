f0dc13f9d690c9f1e161992b3884b1adac63dedd
giridipika <46011548+giridipika@users.noreply.github.com>
2021-07-27 10:35:59 -0500

    Login 1/2 implemented

--- app/src/main/java/com/example/phms/MainActivity.java
+++ app/src/main/java/com/example/phms/MainActivity.java
@@ -1,23 +1,46 @@
 package com.example.phms;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     // Declare all variable here
+    EditText emailID, password;
+    Button btnSignIn;
+    TextView tvSignUp;
+    String email, pwd;
+    private FirebaseAuth userSignIn;
 
-
-    FirebaseAuth mFirebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mFirebaseAuth = FirebaseAuth.getInstance();
+        userSignIn = FirebaseAuth.getInstance();
+
+        btnSignIn = findViewById(R.id.SignInButton);
+        tvSignUp = findViewById(R.id.textViewSignUp);
+
 
     }
 
+
+
 }
+
