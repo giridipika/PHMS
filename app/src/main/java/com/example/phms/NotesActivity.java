c3f4fc2542555b45937366dd8980a70900a77f5c
Santosh <pandeysantosh2021@gmail.com>
2021-08-02 14:41:26 -0500

    note

--- app/src/main/java/com/example/phms/NotesActivity.java
+++ app/src/main/java/com/example/phms/NotesActivity.java
@@ -1,14 +1,109 @@
 package com.example.phms;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.collection.CircularArray;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashSet;
 
 public class NotesActivity extends AppCompatActivity {
+    static ArrayList<String> notes = new ArrayList<>();
+    static  ArrayAdapter arrayAdapter;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if(item.getItemId() == R.id.add_note){
+            Intent intent = new Intent(getApplicationContext(), com.example.phms.NotesEditor.class);
+
+            startActivity(intent);
+
+            return true;
+        }
+        return false;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notes);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.notes", Context.MODE_PRIVATE);
+
+        HashSet<String> set = (HashSet<String>) sharedPreferences.getStringSet("notes", null);
+        if ( set == null){
+            notes.add("Example notes");
+        } else {
+            notes = new ArrayList(set);
+        }
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+                Intent intent = new Intent(getApplicationContext(), com.example.phms.NotesEditor.class);
+                intent.putExtra("noteID",i);
+                startActivity(intent);
+
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int i, long l) {
+
+                final int itemToDelete = i;
+                new AlertDialog.Builder(NotesActivity.this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Are you sure?")
+                        .setMessage("Do you want to delete this note?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        notes.remove(i);
+                                        arrayAdapter.notifyDataSetChanged();
+                                        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.notes", Context.MODE_PRIVATE);
+                                        HashSet<String> set = new HashSet(NotesActivity.notes);
+
+                                        sharedPreferences.edit().putStringSet("notes", set).apply();
+
+                                    }
+                                }
+                        )
+                        .setNegativeButton("No", null)
+                        .show();
+
+                return true;
+            }
+        });
+
     }
 }
