77155295983b5b2dcf98e88608c8d17c5c7863a2
evanpark626 <evan.park626@gmail.com>
2021-08-02 21:57:14 -0500

    Sign Up fixed

--- app/src/main/java/com/example/phms/ui/login/SignUpActivity.java
+++ app/src/main/java/com/example/phms/ui/login/SignUpActivity.java
@@ -2,10 +2,12 @@ package com.example.phms.ui.login;
 
 import android.app.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.content.Intent;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
@@ -25,15 +27,25 @@ import android.widget.Toast;
 import com.example.phms.R;
 import com.example.phms.ui.login.LoginViewModel;
 import com.example.phms.ui.login.LoginViewModelFactory;
+import com.google.firebase.auth.FirebaseAuth;
+import com.example.phms.MainActivity;
+import com.example.phms.HomeActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 
 public class SignUpActivity extends AppCompatActivity {
-
+    FirebaseAuth mFirebaseAuth;
+    TextView SignIn;
     private LoginViewModel loginViewModel;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
         loginViewModel = new ViewModelProvider(this, new LoginViewModelFactory())
                 .get(LoginViewModel.class);
 
@@ -41,6 +53,7 @@ public class SignUpActivity extends AppCompatActivity {
         final EditText passwordEditText = findViewById(R.id.password);
         final Button loginButton = findViewById(R.id.login);
         final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+        SignIn = findViewById(R.id.sign_in);
 
         loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
             @Override
@@ -113,11 +126,49 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 loadingProgressBar.setVisibility(View.VISIBLE);
-                loginViewModel.login(usernameEditText.getText().toString(),
-                        passwordEditText.getText().toString());
+                //loginViewModel.login(usernameEditText.getText().toString(),
+                 //       passwordEditText.getText().toString());
+                String user = usernameEditText.getText().toString();
+                String pswd = passwordEditText.getText().toString();
+                if (user.isEmpty()){
+                    usernameEditText.setError("Please enter Email ID");
+                    usernameEditText.requestFocus();
+                }
+                else if (pswd.isEmpty()){
+                    passwordEditText.setError("Please enter your password");
+                    passwordEditText.requestFocus();
+                }
+                else if (user.isEmpty() && pswd.isEmpty()){
+                    Toast.makeText(SignUpActivity.this, "Fields are empty", Toast.LENGTH_SHORT).show();
+                }
+                else if (!(user.isEmpty() && pswd.isEmpty())){
+                    mFirebaseAuth.createUserWithEmailAndPassword(user, pswd).addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (!task.isSuccessful()){
+                                Toast.makeText(SignUpActivity.this, "Sign Up Unsuccessful, Please Try Again", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                startActivity(new Intent(SignUpActivity.this, HomeActivity.class));
+                            }
+                        }
+                    });
+                }
+                else {
+                    Toast.makeText(SignUpActivity.this, "An Error Occurred", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        SignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(SignUpActivity.this, MainActivity.class);
+                startActivity(i);
             }
         });
     }
+
+
 
     private void updateUiWithUser(LoggedInUserView model) {
         String welcome = getString(R.string.welcome) + model.getDisplayName();
