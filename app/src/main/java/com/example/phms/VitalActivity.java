4d0578da2f73f1ba9d87d0f1cf2dad921be7382b
giridipika <46011548+giridipika@users.noreply.github.com>
2021-08-03 13:31:41 -0500

    VitalSignUpdate1

--- app/src/main/java/com/example/phms/VitalActivity.java
+++ app/src/main/java/com/example/phms/VitalActivity.java
@@ -3,12 +3,34 @@ package com.example.phms;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class VitalActivity extends AppCompatActivity {
+    private TextView tvBP;
+    private TextView tvGL;
+    private TextView tvCH;
+    private Button buttonEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vital);
+
+        tvBP = (TextView) findViewById(R.id.tvBP);
+        tvGL = (TextView) findViewById(R.id.tvGL);
+        tvCH = (TextView) findViewById(R.id.tvCH);
+        buttonEdit = (Button) findViewById(R.id.buttonEdit);
+        buttonEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog();
+            }
+        });
+    }
+    public void openDialog(){
+        VitalDialog vitaldialog = new VitalDialog();
+        vitaldialog.show(getSupportFragmentManager(), "vital dialog");
     }
 }
